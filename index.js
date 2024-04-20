a0ef567cd16e8487d5156a6c272166eb333084b8
laharsh <laharshmoturi@gmail.com>
2024-04-20 16:34:28 -0500

    error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3001;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
